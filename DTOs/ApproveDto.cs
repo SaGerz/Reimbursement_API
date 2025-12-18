f80912096be8621024473c8c23277788936c4880
SaGerz <genarosamuel24@gmail.com>
2025-12-18 23:50:03 +0700

    fix : Update and Fix method approved and rejected

---
+++ DTOs/ApproveDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reimbursement_API.DTOs
+{
+    public class ApproveDto
+    {
+        public string? ManagerApproveNotes {get; set;}
+    }
+}
