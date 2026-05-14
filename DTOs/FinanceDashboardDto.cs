eacdeb90c8c6e9886cfd5407c2827dfac8afbe9c
unknown <genarosamuel24@gmail.com>
2026-05-14 11:28:09 +0700

    fix : update response dashboard finance api

--- DTOs/FinanceDashboardDto.cs
+++ DTOs/FinanceDashboardDto.cs
@@ -7,7 +7,10 @@ namespace Reimbursement_API.DTOs
 {
     public class FinanceDashboardDto
     {
+        public int TotalPendingPayment { get; set; }
+        public decimal TotalAmountPending { get; set; }
+        public int PaidThisMount { get; set; }
         public decimal TotalPaidThisMonth {get; set;}
-        public int TotalPaidCountThisMonth {get; set;}
+        public int? TotalRejectThisMonth { get; set; }
     }
 }
