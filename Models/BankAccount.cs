51bf55057fb410d474495200cd73b36400e53946
unknown <genarosamuel24@gmail.com>
2026-06-25 21:58:25 +0700

    feat : Add 2 new model for disburstment payment feat

---
+++ Models/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reimbursement_API.Models
+{
+    public class BankAccount
+    {
+        [Key]
+        public int BankAccountId { get; set; }
+        
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [Required]
+        public string BankCode { get; set; }
+
+        [Required]
+        public string Accountnumber { get; set; }
+
+        [Required]
+        public string AccountHolderName { get; set; }
+
+        [Required]
+        public bool isActive { get; set; } = true;
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Models/PaymentTransaction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reimbursement_API.Models
+{
+    public class PaymentTransaction
+    {
+        [Key]
+        public int PaymentTransactionId { get; set; }
+        
+        [Required]
+        public int ReimburstmentId { get; set; }
+        [ForeignKey("ReimburstmentId")]
+        public Reimburstment Reimburstment { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public string Status { get; set; }
+        public string? Provider { get; set; }
+        public string? ProviderRefrence { get; set; }
+        public string? FailureReason { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public DateTime? CompleteAt { get; set; }
+
+    }
+}
